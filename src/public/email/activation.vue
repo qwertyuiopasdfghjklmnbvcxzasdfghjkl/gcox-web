edb3dc757d85a374817f9eb79f69830578021ba9
李利军 <13587611202@koall.io>
2019-08-17 18:28:45 +0800

    添加邮件激活页面倒计时
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/public/email/activation.vue
+++ src/public/email/activation.vue
@@ -4,7 +4,7 @@
     <p class="mt45 fs16">{{$t('login_register.acText_1')}}<!-- 账户激活邮件已经发送至 --> <span class="f-c-main">{{$route.query.email}}</span> </p>
     <p class="mt5 fs16">{{$t('login_register.acText_2')}}<!-- 请打开邮件并点击里面提供的链接来激活您的账户。 --><span v-html="$t('login_register.acText_3').format(`“ <span class=f-c-main>noreply@gcox.com</span> ”`)"></span><!-- 如果您未在收件箱看到来自“ <span class="f-c-main">noreply@gcox.com</span> ”的邮件，请再确认一下垃圾邮件。 --></p>
     <p class="mt5 fs16">{{$t('login_register.acText_4')}}<!-- 如未收到账户激活邮件，请点击下面的按钮重新发送 --></p>
-    <p class="mt60"><button type="button" class="mint-btn primary round" :disabled="locked" @click="sendEmail">{{$t('login_register.acResend')}}<!-- 重新发送账户激活邮件 --></button></p>
+    <p class="mt60"><button type="button" class="mint-btn primary round" :disabled="locked" @click="sendEmail">{{$t('login_register.acResend')}}({{this.second}})<!-- 重新发送账户激活邮件 --></button></p>
   </div>
 </template>
 
@@ -13,14 +13,24 @@ import Vue from 'vue'
 import { mapGetters } from 'vuex'
 import userApi from '@/api/user'
 import Config from '@/assets/js/config'
+import utils from '@/assets/js/utils'
 export default {
   data(){
     return {
-      locked:false
+      locked:false,
+      second:'60'
     }
   },
   computed: {
     ...mapGetters(['getLang'])
+  },
+  mounted(){
+    utils.countDown(60, (time)=>{
+      this.second = time.split(':')[1]
+      if(this.second==='00'){
+        this.$router.replace({name:'login'})
+      }
+    })
   },
   methods: {
     sendEmail () {
