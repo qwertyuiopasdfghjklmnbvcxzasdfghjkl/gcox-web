793be34f485a3442baa7e7dbe1b49ba76d4c0ba3
李利军 <13587611202@koall.io>
2019-09-11 17:53:54 +0800

    修改
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,4 +1,4 @@
-let domain = process.env.NODE_ENV === 'development' ? 'exchange-staging.gcox.com' : location.host.split(':')[0]
+let domain = process.env.NODE_ENV === 'development' ? 'gcox.dev.koall.io' : location.host.split(':')[0]
 // let domain = process.env.NODE_ENV === 'development' ? 'dev.koall.io' : location.host.split(':')[0]
 // let domain = process.env.NODE_ENV === 'development' ? 'sit.gcox.cc' : location.host.split(':')[0]
 // let domain = process.env.NODE_ENV === 'development' ? 'sit.koall.io' : location.host.split(':')[0]
@@ -6,7 +6,7 @@ const config = {
   domain: domain,
   origin: `${window.location.protocol}//${domain}`,
   imageType: /\.(jpg|png|jpeg|bmp)/i,
-  protocol: 'wss://'
+  protocol: window.location.protocol === 'https:' ? 'wss://': 'ws://'
 }
 
 export default config
