79f67faec51dc6afe98664f18e8a93b7c74b90d8
李利军 <13587611202@koall.io>
2019-09-19 16:36:59 +0800

    充值memo国际化
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/public/account/adresscava.vue
+++ src/public/account/adresscava.vue
@@ -27,7 +27,7 @@
         <li v-for="item in allData" @click.prevent="icheck(item)" v-show="item.rechargeFlag===1">{{item.symbol}}</li>
       </ul>
     </div>
-    <p class="title-div" style="margin-top: 40px;"><small>{{$t('account.userRechargeAddress')}}<!--充值地址--></small> <small style="color: #E14B26; display: inline-block; background-color: #2E2C3C; padding: 0 8px;" v-show="symbol==='EOS' || symbol==='XRP'"><i class="icon-notification"></i> Memo和Address需要同时使用才能充值成功！</small></p>
+    <p class="title-div" style="margin-top: 40px;"><small>{{$t('account.userRechargeAddress')}}<!--充值地址--></small> <small style="color: #E14B26; display: inline-block; background-color: #2E2C3C; padding: 0 8px;" v-show="symbol==='EOS' || symbol==='XRP'"><i class="icon-notification"></i> {{$t('account.userRechargeMemoTip')}}<!-- Memo和Address需要同时使用才能充值成功！ --></small></p>
     <div class="bottom-box" style="margin-top: 10px;">
       <div class="copy">
         <p class="ellipsis">{{getAddress}}</p>
