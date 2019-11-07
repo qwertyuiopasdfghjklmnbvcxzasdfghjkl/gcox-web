6b832d94d82439022d64209d64b65ae6a5b3575a
李利军 <13587611202@koall.io>
2019-11-07 15:25:33 +0800

    修改
    Signed-off-by: 李利军 <13587611202@koall.io>

--- src/exchanges/market/business.vue
+++ src/exchanges/market/business.vue
@@ -6,7 +6,7 @@
         </div>
         <div class="balance ui-flex ui-flex-justify">
           <div><span>{{isBuy ? baseSymbol : currentSymbol}}</span> <span>{{$t('exchange.exchange_balance')}}<!--余额-->：</span> <span>{{toFixed(isBuy ? toBalance.availableBalance : fromBalance.availableBalance, 8) | removeEndZero | toMoney}}</span></div>
-          <router-link :to="{name:'account_menu', params:{menu: 'adresscava',symbol: currentSymbol}}" class="normal pull-right" tag="button" v-if="getApiToken" :disabled="Number(fromWallet.rechargeFlag) !== 1">{{$t('account.estimated_value_deposit')}}<!-- 充值 --></router-link>
+          <router-link :to="{name:'account_menu', params:{menu: 'adresscava',symbol: currentSymbol}}" class="normal pull-right" tag="button" v-if="getApiToken" :disabled="Number(fromWallet && fromWallet.rechargeFlag) !== 1">{{$t('account.estimated_value_deposit')}}<!-- 充值 --></router-link>
           <router-link :to="{name:'login'}" class="normal pull-right" tag="button" v-else>{{$t('account.estimated_value_deposit')}}<!-- 充值 --></router-link>
         </div>
         <p class="exhange-rate" v-if="false">{{$t('public.fee_rate')}}：{{rateData || 0.01}}%<!--手续费率--></p>
