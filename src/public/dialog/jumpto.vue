b0763448477aaeee3c1d480d2e4970a27b45a0fc
fla126 <fla126@qq.com>
2020-01-07 11:42:04 +0800

    添加ip检测跳转，添加新加坡用户登录跳转
    Signed-off-by: fla126 <fla126@qq.com>

---
+++ src/public/dialog/jumpto.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="dialog">
+        <p class="title">选择一个区</p>
+        <img class="banner" src="../../assets/img/jump_banner.png">
+        <p class="mt60 text-center fs26">转到新加坡地区的网站</p>
+        <p class="mt25 text-center dark">您当前的位置：新加坡</p>
+        <p class="mt15 dark text-center">美洲地区针对美洲的用户进行了优化，为了您的舒适，我们建议您使用你所在地的网站</p>
+        <p class="text-center"><button class="mt40 gobtn" type="button" onclick="location.href = 'https://gcox.sg'">继续→</button></p>
+        <p class="text-center mt70"><a href="javascript:;" @click="closeDialog">留在当前地区的网站上</a></p>
+    </div>
+</template>
+
+<script>
+import userApi from '@/api/individual'
+export default {
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    closeDialog () {
+      this.$emit('removeDialog')
+    },
+  }
+}
+</script>
+
+<style scoped>
+.dark {color: #B6C4CA;}
+.dialog {width: 880px;background-color: #2E2E2E; color: #EDF7FB; padding-bottom: 20px;}
+.title {font-size: 16px; padding: 15px 22px;}
+.banner {display: block; max-width: 100%;}
+.gobtn {line-height: 43px; padding-left: 25px; padding-right: 25px; font-size: 16px; color: #fff; background: linear-gradient(top, #16B6FF,#006593); cursor: pointer;}
+</style>
---
+++ src/public/dialog/jumpto2.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="dialog">
+        <p class="title text-center">即将转到新加坡地区的网站</p>
+        <p class="mt40 text-center dark">您是新加坡用户，根据相关政策要求，请使用<a href="https://gcox.sg" target="_self">GCOX.SG</a>网站</p>
+        <p class="text-center mt80"><button class="gobtn" type="button" onclick="location.href = 'https://gcox.sg'">立即跳转({{time}}S)</button></p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      time:20
+    }
+  },
+  mounted(){
+    let timeOut = () => {
+      this.time--
+      if (this.time === 0) {
+        location.href = 'https://gcox.sg'
+        return
+      }
+      setTimeout(timeOut, 1000)
+    }
+    setTimeout(timeOut, 1000)
+  },
+}
+</script>
+
+<style scoped>
+.dark {color: #B6C4CA;}
+.dialog {width: 880px;background-color: #2E2E2E; color: #EDF7FB; padding-top: 55px; padding-bottom: 65px;}
+.title {font-size: 26px;}
+.gobtn {line-height: 43px; padding-left: 25px; padding-right: 25px; font-size: 16px; color: #fff; background: linear-gradient(top, #16B6FF,#006593); cursor: pointer;}
+</style>
