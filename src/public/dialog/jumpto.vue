77f28d13433a0bf7ec206d790f8dfba6b58d0ecf
fla126 <fla126@qq.com>
2020-01-17 16:02:58 +0800

    优化
    Signed-off-by: fla126 <fla126@qq.com>

--- src/public/dialog/jumpto.vue
+++ src/public/dialog/jumpto.vue
@@ -4,7 +4,7 @@
         <img class="banner" src="../../assets/img/jump_banner.png">
         <p class="mt60 text-center fs26">{{$t(key?'user.goto_main_site':'user.goto_sg_site')}}<!-- 转到GCOX新加坡站点 --></p>
         <p class="mt25 text-center dark">{{$t(key?'user.curr_pos_notsg':'user.curr_pos_sg')}}<!-- 您当前的位置：新加坡 --></p>
-        <p class="mt15 dark text-center">{{$t('user.goto_desc')}}<!-- 美洲地区针对美洲的用户进行了优化，为了您的舒适，我们建议您使用你所在地的网站 --></p>
+        <p class="mt15 dark text-center">{{$t('user.goto_desc').format(key?'GCOX.COM':'GCOX.SG')}}<!-- 美洲地区针对美洲的用户进行了优化，为了您的舒适，我们建议您使用你所在地的网站 --></p>
         <p class="text-center"><button class="mt40 gobtn" type="button" @click="goto">{{$t('user.goto_site').format(key?'GCOX.COM':'GCOX.SG')}}<!-- 继续→ --></button></p>
         <p class="text-center mt70"><a href="javascript:;" @click="closeDialog">{{$t('user.curr_stay')}}<!-- 留在当前地区的网站上 --></a></p>
     </div>
